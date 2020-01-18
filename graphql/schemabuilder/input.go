256c07105ce269f66d551d37abdde14635180e1f
denkhaus <peristaltic@gmx.net>
2020-01-18 20:41:43 +0100

    try

--- graphql/schemabuilder/input.go
+++ graphql/schemabuilder/input.go
@@ -147,7 +147,10 @@ func (sb *schemaBuilder) collectFields(typ reflect.Type, fields map[string]argFi
 	for i := 0; i < typ.NumField(); i++ {
 		field := typ.Field(i)
 		if field.Anonymous {
-			return sb.collectFields(field.Type, fields, argType)
+			if err := sb.collectFields(field.Type, fields, argType); err != nil {
+				return err
+			}
+			continue
 		}
 
 		fieldInfo, err := parseGraphQLFieldInfo(field)
